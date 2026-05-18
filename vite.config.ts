8cdae9289c0de194dae0a25b1f9467f5278a52e6
Omar Qattam <65464415+OmarQatt@users.noreply.github.com>
2026-05-18 15:26:33 +0300

    Add allowed host for Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
+     allowedHosts: ['capture-circle-pro.onrender.com'],
     port: 8080,
     hmr: {
       overlay: false,
